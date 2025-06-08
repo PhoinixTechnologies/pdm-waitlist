b7e97b0e9691560ff6a1d9431f9351a42df639d9
Ife-Banks <Ifeoluwa.bankole05@gmail.com>
2025-06-09 00:35:02 +0100

    Perfected the nav animation ,body animation left

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -52,16 +52,67 @@ const linkHoveAnimate ={
   }
 }
 
+const dropDownAnimate = {
+  initial: {
+    height:0,
+    opacity:0.6
+  },
+  visible: {
+    height:400,
+    opacity:1,
+    transition: {
+      duration:1.0
+    }
+  },
+  exit:{
+    height:0,
+    opacity:0,
+    transition:{
+      when:"beforeChildren"
+    }
+  }
+}
+
+const linkAnimate = {
+  initial: {
+    height:0
+  },
+  visible:{
+    height:100
+  },
+  exit:{
+    height:0,
+    opacity:0
+  }
+}
+const joinAnimate = {
+  initial: {
+    height:0
+  },
+  visible:{
+    height:50
+  },
+  exit:{
+    height:0,
+    opacity:0
+  }
+}
+
+
 const Navbar = () => {
-    const [isvisible, setIsvisible] = useState(false)
+    const [isvisible, setIsvisible] = useState(false);
+    const linkVisibility = () => {
+        setIsvisible(!isvisible)
+    }
     
   return (
-    <motion.nav
+   <AnimatePresence>
+       <motion.nav
     variants={navAnimate}
     initial='initial'
     animate='visible'
-    className="bg-black/60 flex flex-col md:flex-row  md:justify-between md:items-center py-3 px-10 absolute inset-x-0 top-0 h-max">
-      <div className='flex justify-between  items-center'>
+    className=" bg-black/60 flex flex-col md:flex-row  md:justify-between md:items-center py-3  absolute inset-x-0 top-0 h-15">
+      <div className='flex justify-between px-10   items-center'>
       <div className="text-white flex items-center gap-3 h-max text-lg font-semibold max-w-50">
           <LazyLoadImage src={logo} alt="" className='w-12' />
           <a href="#"><motion.h3 
@@ -69,7 +120,7 @@ const Navbar = () => {
           whileHover='hover'
           className='font-logo tracking-wide hidden md:block'>My PDM Tutor</motion.h3></a>
         </div>
-        <div className='flex' className='border' onClick={() => setIsvisible(!isvisible)}>
+        <div className='flex' className='border' onClick={linkVisibility}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" id='btn1' className='md:hidden'>
           <rect width="24" height="24" fill="black" fillOpacity="0.32"/>
           <path d="M3 17H21V15H3V17Z" fill="white"/>
@@ -80,16 +131,16 @@ const Navbar = () => {
       <AnimatePresence>
         {isvisible && (
           <motion.div 
-            initial={{opacity:0, y:-30}}
-            animate={{opacity:1, y:0}}
-            exit={{opacity:0, y:-30}}
-            transition={{
-              duration:1.0,
-              type:'tween'
-
-            }}
-            className='flex flex-col md:flex-row items-end md:items-center gap-y-3 gap-x-3 md:min-w-[70%] md:justify-between  md:flex'>
-            <div className=" flex text-white gap-x-10 items-end md:items-center md:justify-center font-normal font-link max-w-80 flex-col md:flex-row gap-y-3 " >
+          variants={dropDownAnimate}
+          initial='initial'
+          animate='visible'
+          exit='exit'
+            className='border mt-2 bg-black/60 px-10 py-2 flex flex-col md:flex-row items-end md:items-center gap-y-3 gap-x-3 md:min-w-[70%] md:justify-between  md:flex'>
+            <AnimatePresence>
+              {isvisible && (
+                 <motion.div 
+                  variants={linkAnimate}
+                className=" overflow-hidden flex text-white gap-x-10 items-end md:items-center md:justify-center font-normal font-link max-w-80 flex-col md:flex-row gap-y-3 " >
                 <motion.a 
                 variants={linkHoveAnimate}
                 whileHover='hover'
@@ -102,13 +153,21 @@ const Navbar = () => {
               variants={linkHoveAnimate}
                 whileHover='hover'
                 href="#connect">Connect with us</motion.a>
-              </div>
-            <Link to="/join" className=" font- flex justify-center bg-white rounded-full px-6 py-3 max-w-40 items-center">Join Waitlist</Link>
-          
+              </motion.div>
+              )}
+            
+            <motion.div 
+            variants={joinAnimate}
+            className='overflow-hidden '>
+              <Link to="/join" className=" font- flex justify-center bg-white rounded-full px-6 py-3 max-w-40 items-center">Join Waitlist</Link>
+            </motion.div>
+          </AnimatePresence>
      </motion.div>
         )}
       </AnimatePresence>
     </motion.nav>
+
+   </AnimatePresence>
   );
 };
 
